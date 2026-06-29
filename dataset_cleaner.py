31e80b766dfea9fa12f100029fb61a9a8ec138cd
Bravo Diego <diego.bravo.contact@gmail.com>
2026-06-29 03:03:46 +0200

    code poster and model ok !

--- dataset_cleaner.py
+++ dataset_cleaner.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv("ESA_CNES - CubeSeat Dataset - data_V2.csv")
+df = pd.read_csv("bayesian-decision-model-for-cubesat-mission\ESA_CNES - CubeSeat Dataset - data_V2.csv")
 
 df['Coût Total (USD)'] = df['Coût Total (USD)'].astype(str).str.replace(',', '.')
 df['Coût Total (USD)'] = pd.to_numeric(df['Coût Total (USD)'], errors='coerce')
@@ -11,19 +11,19 @@ df['Coût Total (USD)'] = pd.to_numeric(df['Coût Total (USD)'], errors='coerce'
 def calculate_clean_complexity(row):
     #base par Type de Mission (Complexité de la charge utile)
     mission = str(row['Mission_Type']).strip()
-    base_scores = {
-        'Technology Demo': 1.0,
-        'Communications': 1.3,
-        'Earth Observation': 1.8,
-        'Military/Defense': 2.7,
-        'Science/Astronomy': 2.7
+    score = 1.0
+    mission_bonus = {
+        "Technology Demo": 0.0,
+        "Communications": 0.3,
+        "Earth Observation": 0.5,
+        "Military/Defense": 0.7,
+        "Science/Astronomy": 1.0,
+        "Deep Space": 2.0
     }
-    
-    # Si le type est "Deep Space", on le traite comme une mission scientifique complexe
-    if mission == 'Deep Space':
-        score = 3.0
-    else:
-        score = base_scores.get(mission, 1.4) # 1.4 par défaut si inconnu
+    for key, value in mission_bonus.items():
+        if key in mission:
+            score += value
+
     
     org = str(row['Organization']).lower()
     mfg = str(row['Manufactured (AIVT) by']).lower()
@@ -32,19 +32,21 @@ def calculate_clean_complexity(row):
     academic = ['university', 'ecole', 'politecnico', 'college', 'instit', 'in-house']
     
     if any(agency in org or agency in mfg for agency in agencies):
-        score += 0.5
+        if "Science/Astronomy" in mission:
+            score += 0.7
+        else:
+            score += 0.5
     elif any(acad in org or acad in mfg for acad in academic):
         # On vérifie que ce n'est pas un projet universitaire mais clé en main chez un industriel
         if 'gomspace' not in mfg and 'clyde' not in mfg and 'nanoavionics' not in mfg:
             score -= 0.2
-        
         
     return round(score, 2)
 
 
 df['Complexity_Score'] = df.apply(calculate_clean_complexity, axis=1)
 
-df_orbites = pd.read_csv("draft\ESA_CNES - CubeSeat Dataset - data.csv")
+df_orbites = pd.read_csv("bayesian-decision-model-for-cubesat-mission\draft\ESA_CNES - CubeSeat Dataset - data.csv")
 df_merged = pd.merge(df, df_orbites[['id', 'Orbit_Type']], on='id', how='outer')
 print(df_merged[['Project', 'Coût Total (USD)', 'Complexity_Score']].head())
-df_merged.to_csv("CubeSat_Dataset_Clean.csv", index=False)
+df_merged.to_csv("bayesian-decision-model-for-cubesat-mission\CubeSat_Dataset_Clean.csv", index=False)
